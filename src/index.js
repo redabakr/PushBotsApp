723a9cf5060fed9639be88f537bae957edb09f7c
Reda Bakr <reda.bakr@gmail.com>
2017-07-27 07:55:59 +0200

    added express server

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dbConfig from './config/db';
+import middlewaresConfig from './config/middlewares';
+
+const app = express();
+
+/**
+* Database
+*/
+dbConfig();
+/**
+* Middlewares
+*/
+middlewaresConfig(app);
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`App listen to port: ${PORT}`);
+  }
+});
