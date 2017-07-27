0da408e9bde5c2459995c3b8fea6629ac5e90144
Reda Bakr <reda.bakr@gmail.com>
2017-07-27 08:03:41 +0200

    added device model

---
+++ src/modules/device/model.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from 'mongoose';
+
+const DeviceSchema = new Schema({
+  token: {
+    type: String,
+    required: true,
+    //minLength: [25, '25 characters long at least'],
+  },
+}, { timestamps: true });
+
+export default mongoose.model('Device', DeviceSchema);
